d83e552f041a81ee52ec7547522a112dc7e65ce8
ducngocnguyen102 <77482548+ducngocnguyen102@users.noreply.github.com>
2023-10-26 15:08:58 -0700

    Add files via upload

---
+++ WipeDevice.ps1
@@ -0,0 +1,25 @@
+﻿# Kill OneDrive to prevent removing data from OneDrive
+Get-Process -Name *onedrive* | Stop-Process -Force
+
+# Remove UserProfile Data
+Remove-Item -Path C:\Users\ -Recurse -Force
+
+$ExcludeList = @(
+    '$Windows.~BT'
+    'PerfLogs'
+    'Program Files'
+    'Program Files (x86)'
+    'Users'
+    'Windows'
+)
+
+# Exclude Windows System Path
+$CDrive_File = Get-ChildItem C:\  | Where {$ExcludeList -notcontains $_.Name}
+
+# Remove non-system files
+$CDrive_File | % {
+    $_ | Remove-Item -Recurse -Force
+}
+
+# Remote Wipe PC
+
